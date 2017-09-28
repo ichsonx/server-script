f9b6fc8dc42870161001415f3920c6227c96eb79
ichsonx <sonxz@qq.com>
2017-09-28 16:30:25 +0800

    ddd

--- ubuntu/common install.sh
+++ ubuntu/common install.sh
@@ -2,5 +2,5 @@
 
 #install vim, git, lrzsz, curl
 echo "===removing vim...\n"
-sudo apt-get remove vim && echo "===install vim...\n" && sudo apt-get install vim && echo "===install git...\n" && sudo apt-get install git && echo "===install lrzsz...\n" && sudo apt-get install lrzsz
+sudo apt-get remove vim && echo "===install vim...\n" && sudo apt-get install -y vim && echo "===install git...\n" && sudo apt-get install git && echo "===install lrzsz...\n" && sudo apt-get install lrzsz
 sudo apt-get install -y curl
