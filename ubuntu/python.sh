b13d703cb7d6507e293a92f7ab457a90b074e849
ichsonx <sonxz@qq.com>
2017-09-30 09:25:36 +0800

    update

--- ubuntu/python.sh
+++ ubuntu/python.sh
@@ -3,4 +3,7 @@
 #ubuntu16默认已经安装Python3.5.2
 #安装pip3
 
-sudo apt-get install -y python3-pip && echo "install selenium ..." && sudo pip3 install selenium
+sudo apt-get install -y python3-pip && \
+echo "install selenium ..." && \
+sudo pip3 install selenium && \
+sudo pip3 install scrapy
