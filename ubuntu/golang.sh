7823b09b3a47f1100a694c2f022875d5062a1e62
ichsonx <sonxz@qq.com>
2017-11-25 09:04:48 +0800

    update 1.9.2

--- ubuntu/golang.sh
+++ ubuntu/golang.sh
@@ -2,8 +2,8 @@
 
 #用source命令执行本脚本
 #安装golang1.9，并且安装glide
-sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz
+sudo wget https://redirector.gvt1.com/edgedl/go/go1.9.2.darwin-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.9.2.darwin-amd64.tar.gz
 sudo mkdir /root/goprojects && sudo mkdir /root/goprojects/bin && sudo mkdir /root/goprojects/pkg && sudo mkdir /root/goprojects/src
 sudo echo "export GOROOT=/usr/local/go" >> /etc/profile
 sudo echo "export GOPATH=/root/goprojects" >> /etc/profile
