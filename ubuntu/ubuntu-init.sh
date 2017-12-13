6a8240bcaf666927be75be73f256fb5af6b77317
sonx <sonxz@qq.com>
2017-12-13 09:54:26 +0800

    update

--- ubuntu/ubuntu-init.sh
+++ ubuntu/ubuntu-init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "ubuntu initing..."
-sudo apt-get update && sudo apt-get -y upgrade && sudo reboot && \
+sudo apt-get update && sudo apt-get -y upgrade &&  \
 sudo echo "alias aget='sudo apt-get'">>/etc/profile && \
 sudo echo "alias ll='sudo ls -lah'">>/etc/profile && \
-sudo echo "alias vi='sudo vim'">>/etc/profile
+sudo echo "alias vi='sudo vim'">>/etc/profile && \
+sudo reboot
