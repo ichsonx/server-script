20eac2a7a5b2cf203281d76c6ff28a32e05880f9
ichsonx <sonxz@qq.com>
2017-12-13 10:19:24 +0800

    update 1.9.2

--- ubuntu/ubuntu-init.sh
+++ ubuntu/ubuntu-init.sh
@@ -5,4 +5,7 @@ sudo apt-get update && sudo apt-get -y upgrade &&  \
 sudo echo "alias aget='sudo apt-get'">>/etc/profile && \
 sudo echo "alias ll='sudo ls -lah'">>/etc/profile && \
 sudo echo "alias vi='sudo vim'">>/etc/profile && \
+sudo echo "alias aget='sudo apt-get'">>~/.profile && \
+sudo echo "alias ll='sudo ls -lah'">>~/.profile && \
+sudo echo "alias vi='sudo vim'">>~/.profile && \
 sudo reboot
