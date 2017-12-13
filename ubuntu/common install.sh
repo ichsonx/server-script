3a42945f5dbda2dde7c92cbe15cd7b0a90e1f28c
sonx <sonxz@qq.com>
2017-12-13 10:00:26 +0800

    update

--- ubuntu/common install.sh
+++ ubuntu/common install.sh
@@ -4,4 +4,6 @@
 echo "===removing vim...\n"
 sudo apt-get remove vim && echo "===install vim...\n" && sudo apt-get install -y vim && echo "===install git...\n" && sudo apt-get install git && echo "===install lrzsz...\n" && sudo apt-get install lrzsz
 sudo apt-get install -y curl
+sudo git config --global user.email "sonxz@qq.com"
+sudo git config --global user.name "sonx"
 git clone https://github.com/ichsonx/server-script.git
