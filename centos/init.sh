0afbd070f2b226fc2063730e3eaff0f8b17ccf3c
ichsonx <sonxz@qq.com>
2018-01-09 11:07:28 +0800

    update 1.9.2

--- centos/init.sh
+++ centos/init.sh
@@ -3,6 +3,9 @@
 yum install wget && \
 yum install git && \
 yum install curl && \
+yum install bzip2 && \
 git config --global user.email "sonxz@qq.com" && \
 git config --global user.name "sonx" && \
-git clone https://github.com/ichsonx/server-script.git
+git clone https://github.com/ichsonx/server-script.git && \
+echo "alias ll='ls -lah'">>/etc/profile && \
+echo "alias vi='vim'">>/etc/profile
--- centos/python.sh
+++ centos/python.sh
@@ -4,4 +4,15 @@ yum install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqli
 yum -y  install  zlib* && \
 wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz && \
 tar -xzvf Python-3.6.4.tgz -C  /tmp && \
-cd  /tmp/Python-3.6.0/
+cd  /tmp/Python-3.6.4/ && \
+./configure --prefix=/usr/local && \
+make && \
+make altinstall && \
+    #这样安装完，会在/usr/local/bin下生成几个可运行程序，其中软链接python3.6到/usr/bin/python3就可以了
+    #如果还有其他需要软链接的，可自行考虑（例如pip3）
+cd /usr/bin && \
+ln -s /usr/local/bin/python3.6 /usr/bin/python3 && \
+ln -s /usr/local/bin/pip3.6 /usr/bin/pip3 && \
+pip3 install selenium && \
+pip3 install scrapy && \
+pip3 install pymongo
