18986c5a19b816088221f76f2f32c618113c2b57
ichsonx <sonxz@qq.com>
2018-01-15 16:41:01 +0800

    update 1.9.2

--- centos/python.sh
+++ centos/python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+    #以下是安装python3以及相应库的
 yum install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel && \
 yum -y  install  zlib* && \
 wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz && \
@@ -15,4 +16,7 @@ ln -s /usr/local/bin/python3.6 /usr/bin/python3 && \
 ln -s /usr/local/bin/pip3.6 /usr/bin/pip3 && \
 pip3 install selenium && \
 pip3 install scrapy && \
-pip3 install pymongo
+pip3 install pymongo && \
+    #centos默认没有安装puthon的pip，以下是安装pip的
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
