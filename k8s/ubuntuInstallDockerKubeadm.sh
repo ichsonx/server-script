c962c8bc567a6f2e6eb34d1e8a837dc62abd76bd
ichsonx <sonxz@qq.com>
2018-01-23 09:24:51 +0800

    update 1.9.2

--- k8s/ubuntuInstallDockerKubeadm.sh
+++ k8s/ubuntuInstallDockerKubeadm.sh
@@ -2,6 +2,7 @@
 
 #   ubuntu安装docker、kubeadm，并且安装其所依赖的其他软件，并设置开机启动
 #   这样是使用kubeadm来安装完整的k8s的集群
+#   参考地址：https://feisky.gitbooks.io/kubernetes/deploy/kubeadm.html#ubuntu。官网也有的。
 
 apt-get update && apt-get install -y apt-transport-https
 #   下面是下载k8s的ubuntu仓库key，虚拟机即使翻墙了也很有可能下载不了，作者就是这种情况
