3d77b43e4b605e38b59525c7464210c46baedbb0
ichsonx <sonxz@qq.com>
2018-01-23 09:18:24 +0800

    update 1.9.2

--- k8s/ubuntuInstallDockerKubeadm.sh
+++ k8s/ubuntuInstallDockerKubeadm.sh
@@ -4,7 +4,7 @@
 #   这样是使用kubeadm来安装完整的k8s的集群
 
 apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
