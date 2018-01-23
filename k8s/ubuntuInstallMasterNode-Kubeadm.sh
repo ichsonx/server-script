ad970d561af8cd67eefe1a24129514e8b8d86443
ichsonx <sonxz@qq.com>
2018-01-23 09:36:46 +0800

    update 1.9.2

--- k8s/ubuntuInstallMasterNode-Kubeadm.sh
+++ k8s/ubuntuInstallMasterNode-Kubeadm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#   使用kubeadm安装master，设置好相应的pod网络地址。
+#   注意：这里使用前需要先disable swap，不然会报错，ubuntu的相关命令： sudo swapoff -a
+
 # --api-advertise-addresses <ip-address>
 # for flannel, setup --pod-network-cidr 10.244.0.0/16
 kubeadm init --pod-network-cidr 10.244.0.0/16 --kubernetes-version latest
