7a01f84092bd6252dc33681947690beda60243bc
ichsonx <sonxz@qq.com>
2018-03-01 17:30:21 +0800

    update 1.9.2

--- centos/golang.sh
+++ centos/golang.sh
@@ -4,7 +4,7 @@
 #安装golang1.9，并且安装glide
 wget https://redirector.gvt1.com/edgedl/go/go1.9.2.darwin-amd64.tar.gz
 tar -C /usr/local -xzf go1.9.2.darwin-amd64.tar.gz
-mkdir /root/goprojects && sudo mkdir /root/goprojects/bin && sudo mkdir /root/goprojects/pkg && sudo mkdir /root/goprojects/src
+mkdir /root/goprojects && mkdir /root/goprojects/bin && mkdir /root/goprojects/pkg && mkdir /root/goprojects/src
 echo "export GOROOT=/usr/local/go" >> /etc/profile
 echo "export GOPATH=/root/goprojects" >> /etc/profile
 echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> /etc/profile
