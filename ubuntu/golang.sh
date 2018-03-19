42baf932ae9650b60510b559cb317916a3480878
ichsonx <sonxz@qq.com>
2018-03-19 15:48:49 +0800

    update 1.9.2

--- ubuntu/golang.sh
+++ ubuntu/golang.sh
@@ -2,16 +2,16 @@
 
 #用source命令执行本脚本
 #安装golang1.9，并且安装glide
-sudo wget https://redirector.gvt1.com/edgedl/go/go1.9.2.darwin-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.9.2.darwin-amd64.tar.gz
+sudo wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
 sudo mkdir /root/goprojects && sudo mkdir /root/goprojects/bin && sudo mkdir /root/goprojects/pkg && sudo mkdir /root/goprojects/src
 sudo echo "export GOROOT=/usr/local/go" >> /etc/profile
 sudo echo "export GOPATH=/root/goprojects" >> /etc/profile
 sudo echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> /etc/profile
-sudo echo "export GOROOT=/usr/local/go" >> ~/.bashrc
-sudo echo "export GOPATH=/root/goprojects" >> ~/.bashrc
-sudo echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> ~/.bashrc
-sudo echo "export GOROOT=/usr/local/go" >> ~/.profile
-sudo echo "export GOPATH=/root/goprojects" >> ~/.profile
-sudo echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> ~/.profile
+#sudo echo "export GOROOT=/usr/local/go" >> ~/.bashrc
+#sudo echo "export GOPATH=/root/goprojects" >> ~/.bashrc
+#sudo echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> ~/.bashrc
+#sudo echo "export GOROOT=/usr/local/go" >> ~/.profile
+#sudo echo "export GOPATH=/root/goprojects" >> ~/.profile
+#sudo echo "export PATH=\$PATH:\$GOROOT:\$GOROOT/bin:\$GOPATH:\$GOPATH/bin" >> ~/.profile
 source /etc/profile
